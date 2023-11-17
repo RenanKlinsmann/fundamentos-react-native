9316652f8ec181622d496c1bc50e911a1aa758d1
Renan Mendonça <klismann-nan@hotmail.com>
2023-11-16 21:00:14 -0300

    Resposta exercicio 03

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,16 +1,34 @@
 import React, {useState} from 'react';
-import { View, TextInput, Text } from 'react-native';
+import { View, TextInput, Button } from 'react-native';
 export default props => {
 
-    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+    const carregaDados = _=> {
+        console.warn(email, senha);
+    }
 
     return (
         <View>
             <TextInput
                 style={{borderWidth: 1.0}}
-                placeholder='Digite seu nome'
-                value={nome}
-                onChangeText={nome => setNome(nome)}
+                keyboardType='email-address'
+                placeholder='Digite seu email'
+                value={email}
+                onChangeText={email => setEmail(email)}
+            />
+             <TextInput
+                style={{borderWidth: 1.0}}
+                secureTextEntry
+                placeholder='Digite sua senha'
+                value={senha}
+                onChangeText={senha => setSenha(senha)}
+            />
+            <Button
+                title="Login"
+                onPress={carregaDados}
+                color={"#008000"}
             />
         </View>
     )
